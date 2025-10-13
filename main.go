764e351467f59d16290746c74cb752503ab9571b
Danil Samsonov <thrashdev@protonmail.com>
2025-10-13 13:17:01 +0600

    added shit

--- main.go
+++ main.go
@@ -1,12 +1,46 @@
 package main
 
 import (
+	// "context"
 	"context"
+	"fmt"
 	"hypermedia/internal/component"
-	"os"
+	"log"
+	"net/http"
+	// "os"
+	// "github.com/a-h/templ"
 )
 
+var dev = true
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("serving root")
+	c := component.Hello("Fredder4")
+	c.Render(context.Background(), w)
+}
+
+func disableCacheInDevMode(next http.Handler) http.Handler {
+	if !dev {
+		return next
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-	c := component.Hello("John")
-	c.Render(context.Background(), os.Stdout)
+	// c := component.Hello("John")
+	serveMux := http.NewServeMux()
+	serveMux.HandleFunc("/", handleRoot)
+	serveMux.Handle("/assets/",
+		disableCacheInDevMode(
+			http.StripPrefix("/assets", http.FileServer(http.Dir("assets")))))
+	server := http.Server{Handler: serveMux, Addr: ":8080"}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// c.Render(context.Background(), os.Stdout)
 }
--- start.bat
+++ start.bat
@@ -1,1 +1,17 @@
-mingw32-make -f .\win_make live
+@echo off
+wt ^
+    --title "Templ Watch" cmd.exe /k "templ generate --watch --proxy=\"http://localhost:8080\" --open-browser=false -v --cmd=\"go run .\"" ^
+; ^
+    --title "Go Server (Air)" cmd.exe /k "go.exe run github.com/air-verse/air@v1.63.0 --build.cmd \"go.exe build -o tmp/bin/main.exe\" --build.bin \"tmp/bin/main.exe\" --build.delay \"100\" --build.exclude_dir \"node_modules\" --build.include_ext \"go\" --build.stop_on_error \"false\" --misc.clean_on_exit true" ^
+; ^
+    --title "Tailwind Watch" cmd.exe /k "pnpx @tailwindcss/cli -i ./input.css -o ./assets/css/styles.css --minify --watch" ^
+; ^
+    --title "Asset Reloader (Air)" cmd.exe /k "go.exe run github.com/air-verse/air@v1.63.0 --build.cmd \"templ.exe generate --notify-proxy\" --build.bin \"" --build.delay \"100\" --build.exclude_dir \"\" --build.include_dir \"assets\" --build.include_ext \"js,css\""
+    REM --title "Asset Reloader (Air)" cmd.exe /k "go.exe run github.com/air-verse/air@v1.63.0 --build.cmd \"templ.exe generate --notify-proxy\" --build.bin \"cmd.exe /c echo 1\" --build.delay \"100\" --build.exclude_dir \"\" --build.include_dir \"assets\" --build.include_ext \"js,css\""
+
+
+REM
+REM wt ^
+REM     --title "Templ watch" cmd.exe /k "templ generate --watch --proxy="http://localhost:8080" --open-browser=false -v" ^
+REM ;   --tab ^
+REM     --title "air"
