c670480397c882fc3e43ce36bdbaa0c9aae76949
Danil Samsonov <thrashdev@protonmail.com>
2025-11-03 19:50:36 +0600

    wrapped in tx

--- internal/database/tx_payment_plans.go
+++ internal/database/tx_payment_plans.go
@@ -3,18 +3,29 @@ package database
 import (
 	"context"
 	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func (q *Queries) CreatePaymentPlan(ctx context.Context, arg InsertPaymentPlanParams) (PaymentPlan, error) {
-	err := q.EndLastPlan(ctx, arg.HouseID)
+func (q *Queries) CreatePaymentPlan(ctx context.Context, pool *pgxpool.Pool, arg InsertPaymentPlanParams) (PaymentPlan, error) {
+
+	tx, err := pool.Begin(ctx)
+	if err != nil {
+		return PaymentPlan{}, fmt.Errorf("Failed to start transaction: %v\n", err)
+	}
+	defer tx.Rollback(ctx)
+	qtx := q.WithTx(tx)
+
+	err = qtx.EndLastPlan(ctx, arg.HouseID)
 	if err != nil {
 		return PaymentPlan{}, fmt.Errorf("Failed to end last plan: %v\n", err)
 	}
 
-	pp, err := q.InsertPaymentPlan(ctx, arg)
+	pp, err := qtx.InsertPaymentPlan(ctx, arg)
 	if err != nil {
 		return PaymentPlan{}, fmt.Errorf("Failed to create a payment plan: %v\n", err)
 	}
 
+	tx.Commit(ctx)
 	return pp, nil
 }
